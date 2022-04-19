d83c260bf86092090569c65a290c4f98bff753c3
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-04-19 10:40:32 +0200

    Add project files.

---
+++ WebApi_TodoList/Controllers/TodoListController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_TodoList.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class TodoListController : ControllerBase
+    {
+        //TODO add POST function
+        //TODO GET single TODO item
+        //TODO implement CRUD (create, read, update, delete)
+
+        [Route("todos")]
+        [HttpGet]
+        public List<string> getTodos(int? count)
+        {
+            int c = count ?? 3;
+
+            return new List<string>
+            {
+                "handla mjölk",
+                "fixa grejer",
+                "göra saker"
+            }.Take(c)
+                .ToList();
+        }
+    }
+
+}
